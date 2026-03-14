34d598384b4ce52b576b594ba7d511d8b32d4c89
annek77 <anne.kinigadner@gmail.com>
2026-03-14 11:26:52 +0100

    Create palindromeData with categorized palindromes
    Added a list of English palindromes grouped by difficulty.

---
+++ think/typing_garden/js/data-palindromes.js
@@ -0,0 +1,20 @@
+// Palindrom word lists (real English palindromes) grouped by difficulty
+export const palindromeData = {
+  short: [
+    'aha', 'bib', 'bob', 'cec', 'dad',
+    'did', 'eve', 'eye', 'gag', 'kayak',
+    'level', 'madam', 'mom', 'noon', 'radar'
+  ],
+  medium: [
+    'redder', 'reviver', 'rotator', 'repaper', 'racecar',
+    'deified', 'hannah', 'civic', 'leveler', 'sexes',
+    'sagas', 'solos', 'tenet', 'stats', 'minim',
+    'denned', 'revver', 'selles', 'sesses', 'succus'
+  ],
+  long: [
+    'redivider', 'detartrated', 'rotavator',
+    'tattarrattat', 'aibohphobia', 'malayalam',
+    'deleveled', 'evitative', 'kinnikinnik',
+    'releveler'
+  ]
+};
